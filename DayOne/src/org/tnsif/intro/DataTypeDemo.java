91542ff6d02e77a6f71768950de772f49d4e3f00
SAHIL WAGH <SAHIL WAGH@Cristyy>
2023-02-02 13:01:44 +0530

    DayOne Second Commit

--- DayOne/src/org/tnsif/intro/DataTypeDemo.java
+++ DayOne/src/org/tnsif/intro/DataTypeDemo.java
@@ -5,7 +5,24 @@ public class DataTypeDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
+		byte a = 127;
+		int b = 127;
+		long b1 = 24544646;
+		float c = 64;
+		double d = 545.69;
+		
+		System.out.println("Value of a is : "+a);
+		System.out.println("Value of b is : "+b);
+		System.out.println("Value of c is : "+c);
+		System.out.println("Value of d is : "+d);
+		
+		int x = 'c';
+		System.out.println("Value : "+x);  //ASCII value of that alphabet is returned 
+		
+		
+		String Name = "sahil";
+		System.out.println("NAME : "+Name);
+		
 	}
 
 }
---
+++ DayOne/src/org/tnsif/intro/UserInputDemo.java
@@ -0,0 +1,39 @@
+package org.tnsif.intro;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//System.out.println("enter value of x");
+		//int x = sc.nextInt();
+		
+		//System.out.println("enter value of y");
+		//float y = sc.nextFloat();
+		
+		//System.out.println("enter value of z");
+		//char z = sc.next().charAt(0);
+		
+		//System.out.println("enter value of name");
+		//String name = sc.next();
+		
+		System.out.println("enter value of str");
+		String str = sc.nextLine();
+		
+		//System.out.println("enter value of d");
+		//double d = sc.nextDouble();
+		
+		//System.out.println("value of x is"+x);
+		//System.out.println("value of y is"+y);
+		//System.out.println("value of z is"+z);
+		//System.out.println("value of name is"+name);
+		System.out.println("value of str is"+str);
+		//System.out.println("value of d is"+d);
+		
+	}
+
+}
